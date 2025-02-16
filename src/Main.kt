e10877bab197cf46cd7165600285406d75372670
BarrakBM <barrak_mandani@live.com>
2025-02-16 17:57:37 +0300

    Task is completed with the challenge

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,18 @@
 // Task 1
-class Book constructor(){
-    var title: String =""
-    var author: String = ""
-    var yearPublished: Int = 0
+class Book constructor(var title: String, var author: String, var yearPublished: Int, var genre: String){
+//    var title: String =""
+//    var author: String = ""
+//    var yearPublished: Int = 0
+    fun getBookInfo(){
+        println("$title by $author ($yearPublished) genre: $genre" )
+    }
 
 }
 
 fun main() {
     // Task 1
-    val book1 = Book()
-    book1.title = "Atomic habits"
-    book1.author = "James Clear"
-    book1.yearPublished = 2018
+    val book1 = Book("Atomic habits", "James Clear", 2018, "self help book")
+    book1.getBookInfo()
 
     // Task 2
 
