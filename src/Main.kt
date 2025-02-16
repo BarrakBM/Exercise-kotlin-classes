15460762965e780cea129008ced37459e7cad428
BarrakBM <barrak_mandani@live.com>
2025-02-16 17:29:54 +0300

    task 1 completed

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
+// Task 1
+class Book constructor(){
+    var title: String =""
+    var author: String = ""
+    var yearPublished: Int = 0
+
+}
+
 fun main() {
-    println("Hello World!")
+    // Task 1
+    val book1 = Book()
+    book1.title = "Atomic habits"
+    book1.author = "James Clear"
+    book1.yearPublished = 2018
+
+    // Task 2
+
 }
